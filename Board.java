82e266edf842c117b8f93b1a2cf0529138bc2050
Melissa Chauffour <myc847@cs.bham.ac.uk>
2019-03-08 18:40:33 +0000

    Upload New File

---
+++ Board.java
@@ -0,0 +1,66 @@
+
+public class Board extends ??? {
+	private int width; 
+	private int height;
+	private int rows;
+	private int columns;
+	private int size;
+
+  public Grid(int w, int h, int r, int c) {
+  	this.width = width;
+  	this.height = width;
+  	this.rows = rows;
+  	this.columns = columns;
+  	this.size = size;
+  }
+  
+  public void setWidth(int Width) {
+        this.Width = Width;
+    }
+  
+  public void setHeight(int Height) {
+        this.Height = Height;
+    }
+  
+  public void setRows(int Rows) {
+        this.Rows = Rows;
+    }
+  
+  public void setColumns(int Columns) {
+        this.Columns = Columns;
+    }
+  
+  public void setSize(int Size) {
+        this.getWidth() * getHeight();
+    }
+
+    (comment) - creating rows
+    int rowHeight = height / rows;
+    for (i = 0; i < rows; i++)
+      g.drawLine(0, i * rowHeight, widthHeight, i * rowHeight);
+
+  	(comment) - creating columns
+    int rowWidth = width / columns;
+    for (i = 0; i < columns; i++)
+      g.drawLine(i * rowWidth, 0, i * rowWidth, height);
+}
+
+  public void placeShipsRandomly(){
+        Random r = new Random();
+        int[] position;
+        int x = length
+        int y = height
+        Orientation orientation;
+        
+        Ship[] ships = new Ship[5];
+        
+        ships[0] = new Zeus();
+        ships[1] = new Sledghammer();
+        ships[2] = new Stellar();
+        ships[3] = new Ajax();
+        ships[4] = new Vector();
+        
+        for(int i = 0; i<5; i++)
+                position = 'insert game name'.getRandomInput();
+  }              
+                
