57f6a88da4fa38f6ff361b47253b22b4013f5532
mag815 <mag815@cs.bham.ac.uk>
2019-03-19 17:30:35 +0000

    updated tests

--- MainGame/src/Tests/GameTest.java
+++ MainGame/src/Tests/GameTest.java
@@ -1,5 +1,6 @@
 package Tests;
 
+
 import static org.junit.Assert.*;
 
 import Game.Gameboard;
@@ -7,13 +8,16 @@ import Game.Ship;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 
 public class GameTest {
 
 	
 	private Gameboard expected;
 	private Ship ship1, ship2, ship3, ship4;
-
+	private ArrayList<String> locations;
 
 	
 	
@@ -29,6 +33,16 @@ public class GameTest {
 		expected.addShip(ship2);
 		expected.addShip(ship3);
 		expected.addShip(ship4);
+
+
+		ArrayList<String> locations = new ArrayList<>();
+		locations.add("1");
+		locations.add("2");
+		locations.add("3");
+		locations.add("4");
+
+
+
 	}
 	
 	@Test
@@ -43,10 +57,7 @@ public class GameTest {
 		
 		
 		
-		
-
-		
-		assertEquals(expected.getBoard().size(), gameboard.getBoard().size());
+		assertEquals(4, gameboard.getBoard().size());
 	
 	}
 	
@@ -55,12 +66,39 @@ public class GameTest {
 	{
 		Gameboard gameboard = new Gameboard();
 		gameboard.addShip(ship1);
-		gameboard.removeShip(ship1);
-		gameboard.removeShip(ship2);
+		gameboard.addShip(ship2);
+		gameboard.addShip(ship3);
+		gameboard.addShip(ship4);
+		gameboard.removeShip(ship3);
 		
-		assertEquals(expected.getBoard().size(), gameboard.getBoard().size());
+		assertEquals(3, gameboard.getBoard().size());
 	}
 
-	
-	
+
+	@Test
+	public void setLocationTest()
+	{
+		Gameboard gameboard = new Gameboard();
+
+		ship1.setLocation(locations);
+
+
+		assertEquals(locations, ship1.getLocation());
+	}
+
+
+	@Test
+	public void setTypeTest() {
+		Gameboard gameboard = new Gameboard();
+
+		ship1.setType("1");
+
+		assertEquals("1", ship1.getType());
+		assertEquals("2", ship2.getType());
+		assertEquals("3", ship3.getType());
+		assertEquals("4", ship4.getType());
+
+	}
+
+
 }
