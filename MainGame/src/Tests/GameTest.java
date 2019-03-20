f85a87cc36e55b8722c76a9f86c6e7b25b701244
James Bramley <james.bramley@outlook.com>
2019-03-20 15:34:04 +0000

    Updating ship class to process attempts correctly. Tests changed to reflect this

--- MainGame/src/Tests/GameTest.java
+++ MainGame/src/Tests/GameTest.java
@@ -186,8 +186,7 @@ public class GameTest {
         assertEquals("HIT", ship1.attempt("2"));
         assertEquals("HIT", ship1.attempt("3"));
         assertEquals("HIT", ship1.attempt("4"));
-        assertEquals("HIT", ship1.attempt("5"));
-
+        assertEquals("DESTROYED", ship1.attempt("5"));
 
     }
 
