7ea86031a6ae7a9614e800a1189f44c592d15a35
James Bramley <james.bramley@outlook.com>
2019-03-23 19:30:46 +0000

    DatabaseRunner class currently prints out the leaderboard to check it works

--- MainGame/src/Database/DatabaseRunner.java
+++ MainGame/src/Database/DatabaseRunner.java
@@ -1,19 +1,13 @@
 package Database;
 
-import java.util.Random;
+import java.util.ArrayList;
 
 public class DatabaseRunner {
     public static void main(String[] args) {
         DatabaseManager db = new DatabaseManager();
-        for (int i = 0; i <= 100; i++) {
-            Random rand = new Random();
-            int player1 = rand.nextInt(30);
-            int player2 = rand.nextInt(30);
-            while (player2 == player1)  {
-                player2 = rand.nextInt(30);
-            }
-            db.updateGameHistory(true,"player" + player1 );
-            db.updateGameHistory(false, "player"+player2);
+        ArrayList<PlayerData> results = db.calculateLeaderBoard();
+        for (PlayerData player : results) {
+            System.out.println(player.getUsername() + " " + player.getWinratio());
         }
     }
 }
